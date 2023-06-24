6123c489c64e8be734eedc41d84e7ecbf402954e
unknown <jvelizc12@miumg.edu.gt>
2023-06-24 10:30:55 -0600

    Segundo Proceso

--- src/recuperacionjoseveliz/ClaseHijaFinal.java
+++ src/recuperacionjoseveliz/ClaseHijaFinal.java
@@ -3,5 +3,62 @@ package recuperacionjoseveliz;
 import java.util.Scanner;
 
 public class ClaseHijaFinal {
-}
+
+
+    private String nombreHija;
+    private int cantidadEjercicios;
+
+    public void setNombreHija(String nombre) {
+        this.nombreHija = nombre;
+    }
+
+    public String getNombreHija() {
+        return nombreHija;
+    }
+
+    public void setCantidadEjercicios(int cantidad) {
+        this.cantidadEjercicios = cantidad;
+    }
+
+    public int getCantidadEjercicios() {
+        return cantidadEjercicios;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        ClaseHijaFinal hija = new ClaseHijaFinal();
+
+   
+        System.out.print("Ingrese el nombre de la hija: ");
+        String nombre = scanner.nextLine();
+        hija.setNombreHija(nombre);
+
+    
+        System.out.print("Ingrese la cantidad de ejercicios: ");
+        int cantidad = scanner.nextInt();
+        hija.setCantidadEjercicios(cantidad);
+
+       
+        System.out.println("Nombre de la hija: " + hija.getNombreHija());
+        System.out.println("Cantidad de ejercicios: " + hija.getCantidadEjercicios());
+    
+        int[] numeros = {2, 3, 4, 5, 6, 7};
+        int sumaImpares = sumarImpares(numeros);
+        System.out.println("La suma de los números impares es: " + sumaImpares);
+    }
+
+    public static int sumarImpares(int[] numeros) {
+        int suma = 0;
+        int i = 0;
+        while (i < numeros.length) {
+            if (numeros[i] % 2 != 0) {
+                suma += numeros[i];
+            }
+            i++;
+        }
+        return suma;
+    }
+        }
 
